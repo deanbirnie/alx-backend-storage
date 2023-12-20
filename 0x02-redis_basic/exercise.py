8ac8a40902333dfdde28c5ebd1f90b9d48aefd2c
deanbirnie <deanicdl@gmail.com>
2023-12-20 19:00:03 +0000

    Task 1 - Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ reads and writes for caching and storing data
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -21,6 +21,39 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Takes in data and returns a randomly generated key
+        which is used to store the data
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, None]:
+        """
+        This method is used to convert the data back to the desired
+        format from the string format Redis stores it as.
+        """
+        result = self._redis.get(key)
+        if result is None:
+            return None
+        if fn:
+            return fn(result)
+        return result
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Parametrize Cache.get with the correct conversion function for
+        strings.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8") if isinstance(
+            d, bytes
+        ) else d)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Parametrize Cache.get with the correct conversion function for
+        integers.
+        """
+        return self.get(key, fn=int)

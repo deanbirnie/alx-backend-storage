2bb07d2f919fcc5517f020814baffe0fe753a342
deanbirnie <deanicdl@gmail.com>
2023-12-20 20:29:42 +0000

    Task 3 - Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,6 +20,22 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+        """
+        Decorator to store the history of inputs and outputs for a function.
+        """
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            input_key = f"{method.__qualname__}:inputs"
+            output_key = f"{method.__qualname__}:outputs"
+
+            self._redis.rpush(input_key, str(args))
+            output = method(self, *args, **kwargs)
+
+            self._redis.rpush(output_key, output)
+            return output
+        return wrapper
+
 
 class Cache:
     """
@@ -34,6 +50,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Takes in data and returns a randomly generated key

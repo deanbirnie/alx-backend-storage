a7dfae7ed6c169f2af39f39a6f6c3282c0e7f307
deanbirnie <deanicdl@gmail.com>
2023-12-20 20:35:14 +0000

    Task 4 - Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,35 +20,37 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
 def call_history(method: Callable) -> Callable:
-        """
-        Decorator to store the history of inputs and outputs for a function.
-        """
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            input_key = f"{method.__qualname__}:inputs"
-            output_key = f"{method.__qualname__}:outputs"
+    """
+    Decorator to store the history of inputs and outputs for a function.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
 
-            self._redis.rpush(input_key, str(args))
-            output = method(self, *args, **kwargs)
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
 
-            self._redis.rpush(output_key, output)
-            return output
-        return wrapper
+        self._redis.rpush(output_key, output)
+        return output
+    return wrapper
+
 
 def replay(method: Callable):
-        """
-        Display the history of calls of a particular function
-        """
-        method_name = method.__qualname__
-        inputs = cache._redis.lrange(f"{method_name}:inputs", 0, -1)
-        outputs = cache._redis.lrange(f"{method_name}:outputs", 0, -1)
+    """
+    Display the history of calls of a particular function
+    """
+    method_name = method.__qualname__
+    inputs = cache._redis.lrange(f"{method_name}:inputs", 0, -1)
+    outputs = cache._redis.lrange(f"{method_name}:outputs", 0, -1)
 
-        print(f"{method_name} was called {len(inputs)} times:")
-        for input_args, output in zip(inputs, outputs):
-            input_str = input_args.decode()
-            output_str = output.decode()
-            print(f"{method_name}(*{input_str}) -> {output_str}")
+    print(f"{method_name} was called {len(inputs)} times:")
+    for input_args, output in zip(inputs, outputs):
+        input_str = input_args.decode()
+        output_str = output.decode()
+        print(f"{method_name}(*{input_str}) -> {output_str}")
 
 
 class Cache:
